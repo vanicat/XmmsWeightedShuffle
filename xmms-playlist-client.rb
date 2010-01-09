46201edb53e6149c60a2b14a1bc7c931b75dae90
Rémi Vanicat <vanicat@debian.org>
2010-01-09 13:37:15 +0100

    The collections we used to make the weighted shuffled.

--- xmms-playlist-client.rb
+++ xmms-playlist-client.rb
@@ -19,4 +19,41 @@ xmms.add_to_glib_mainloop
 
 ml = GLib::MainLoop.new(nil, false)
 
+# will be read in configuration in some futur time
+cols_configs = [
+  { "name" => "1-rated", "mult" => 0 },
+  { "name" => "2-rated", "mult" => 1 },
+  { "name" => "3-rated", "mult" => 2 },
+  { "name" => "4-rated", "mult" => 4 },
+  { "name" => "5-rated", "mult" => 8 },
+  { "name" => "not-rated", "mult" => 2 }
+]
+
+class Collection
+  attr_reader :coll, :mult, :count
+  def initialize(coll_val, mult_val,count_val)
+    coll = coll_val
+    mult = mult_val
+    count = count_val
+  end
+end
+
+mycols = []
+
+cols_configs.each do |v|
+  xmms.coll_get(v["name"]) do |coll|
+    if(coll.is_a?(Xmms::Collection)) then
+      xmms.coll_query_ids(coll) do |ids_list|
+        mycols.push( Collection.new(coll, ids_list.length, v["mult"]) )
+        false
+      end
+    else
+      puts "Problem with collection #{v["name"]}"
+      puts "Please make sure it exists."
+      exit
+    end
+    false
+  end
+end
+
 ml.run
